b5ff6cb66765ceed4a431373d3878290efb80a4c
Dragos <nedeleadragos5@gmail.com>
2025-04-30 19:35:21 +0300

    map fix test 5

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Your existing JS code here
   
-  fetch('/trackngo/resources/pics/md.svg')
+  fetch('./resources/pics/md.svg')
     .then(res => res.text())
     .then(svg => {
       document.getElementById('svg-container').innerHTML = svg;
